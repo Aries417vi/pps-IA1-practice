ac1b31c7b7fdf5c7266ab077910b23d628c246b3
aries <94883201+Aries417vi@users.noreply.github.com>
2022-01-31 16:12:03 +0000

    p1original

--- p1original.c
+++ p1original.c
@@ -1,2 +1,26 @@
+#include <stdio.h>
 
+void input(int *pa,int *pb)
+{
+  printf("Enter two numbers:\n");
+  scanf(" %d%d ",pa,pb);
+}
+
+void add(int a,int b,int *sum)
+{
+  *sum = a + b;
+}
+
+void output(int a,int b,int sum)
+{
+  printf("the sum of two numbers %d+%d is %d",a,b,sum );
+}
+
+int main()
+{
+int a,b,c;
+input(&a,&b);
+add(a,b,&c);
+output(a,b,c);
+return 0;
 }
